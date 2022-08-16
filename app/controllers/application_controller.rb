abd12de5a0d01fdd9b91505753a25f35f1597a40
itigoame <rikuan1128@gmail.com>
2022-08-16 13:08:55 +0000

    [add]model,controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,8 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up,keys:[:name])
   end
 
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up,keys:[:email])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+
+  get 'users/show'
+  get 'users/edit'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   devise_for :users
   root to: 'homes#top'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "homes/about" => "homes#about", as: "about"
+  resources :books,only: [:new,:index,:show,:edit]
+  resources :users,only: [:show,:edit]
+
 end
