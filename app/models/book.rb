cc81fae6b04f41317c8f501a475508a46583abce
itigoame <rikuan1128@gmail.com>
2022-08-18 03:21:38 +0000

    [add]homeview

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,22 @@
 class Book < ApplicationRecord
 
-  belongs_to :user
+    belongs_to :user
+
+    def index
+      @book = Book.new
+      @books = Books.all
+    end
+
+    def create
+      book = Book.new(book_params)
+      book.save
+      redirect_to "/"
+    end
+
+    private
+
+    def book_params
+      params.require(:book).permit(:title,:body)
+    end
+
 end
